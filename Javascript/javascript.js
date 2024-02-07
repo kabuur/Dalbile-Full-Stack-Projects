51bdd6931ba7b41e25134471d2164b2714ac17fa
Ahmed Sahal <kabuur7@gmail.com>
2024-02-07 10:06:17 -0800

    Dalbile full stack

---
+++ Javascript/javascript.js
@@ -0,0 +1,12 @@
+// alert("hello")
+
+document.write("hhhhhhh")
+
+const h1 = document.getElementById('Myh1').style.color = "red";
+document.getElementById('Myh2').innerHTML=" i am ahmed shal"
+
+document.write("<h1>hello jjjjj</h1>")
+        
+
+
+// document.getElementById("myH1").style.color = "red";
