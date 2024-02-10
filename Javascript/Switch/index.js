35345317625ed21b046aac87f625098b251aed89
Ahmed Sahal <kabuur7@gmail.com>
2024-02-10 09:11:11 -0800

    dalbile fulstack/

---
+++ Javascript/Switch/index.js
@@ -0,0 +1,55 @@
+// let num = document.getElementById('input')
+// let btn = document.getElementById('btn').style.backgroundColor = `"red`
+
+
+// btn.addEventListener("click", ()=> {
+//     console.log('input')
+// })
+
+
+// let day = "Sunday";
+
+// switch (day) {
+//     case "Sunday":
+//         console.log(`to day is ${day}`);
+//     break
+//     case "Sunday":
+//         console.log(`to day is ${day}`);
+//     break
+//     case "Monday":
+//         console.log(`to day is ${day}`);
+//     break
+//     case "fraday":
+//         console.log(`to day is ${day}`);
+//     break
+//     default:
+//         console.log(`Unknown  ${day}`);
+// }
+
+
+
+// let num = 0
+
+// // let name =  prompt("enter your name" +num)
+// while (num <= 20){
+//     if (num % 2 == 0){
+//         document.write(`${num} <br>`)
+//     }
+ 
+//    num++
+// }
+
+
+
+// for (i = 0; i <=20; i++){
+//     document.write(`${i} <br>`)
+// }
+
+
+
+let num = 0
+do{
+    num ++
+    document.write(`${num} <br>`)
+   
+} while (num <= 25)
